fd480497ae9caa77e926c55c6a5f10873d8959ea
mgphone <pmn.blazer@gmail.com>
2025-06-24 23:54:04 +0100

    Create input field and parse comma-separated usernames

--- index.mjs
+++ index.mjs
@@ -3,6 +3,27 @@
 // You can delete the contents of the file once you have understood how it
 // works.
 
-export function makeFetchRequest() {
-  return fetch("https://example.com/test");
-}
+// export function makeFetchRequest() {
+//   return fetch("https://example.com/test");
+// }
+
+const fetchBtn = document.getElementById("fetchBtn");
+const usernamesInput = document.getElementById("usernames");
+const languageSelectorDiv = document.getElementById("languageSelector");
+const languageSelect = document.getElementById("languageSelect");
+const leaderboardTable = document.getElementById("leaderboardTable");
+const leaderboardBody = leaderboardTable.querySelector("tbody");
+let userData = []; //this is for userDataStorage array type
+fetchBtn.addEventListener("click", async () => {
+  const rawUserName = usernamesInput.value.trim();
+  if (!rawUserName) {
+    alert("Please Enter At Lease One UserName");
+    return;
+  }
+  const usernames = rawUserName
+    .split(",")
+    .map((ele) => ele.trim())
+    .filter(Boolean);
+  fetchBtn.disabled = true;
+  fetchBtn.textContent = "Fetching.....";
+});
